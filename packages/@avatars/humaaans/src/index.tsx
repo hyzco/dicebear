8165ca22bc6114b4fe0a55cc001377b757044d65
Florian Körner <contact@florian-koerner.com>
2020-08-04 23:09:56 +0200

    Add: humaaans, open-peeps and personas packages

---
+++ packages/@avatars/humaaans/src/index.tsx
@@ -0,0 +1,11 @@
+import { IStyle } from '@avatars/core';
+
+const style: IStyle<any> = function (prng, options = {}) {
+  return `
+    <svg viewBox="-8 0 280 280" fill="none" xmlns="http://www.w3.org/2000/svg">
+      
+    </svg>
+  `;
+};
+
+export default style;
