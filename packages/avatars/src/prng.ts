6161a46b30c5ca4abde1797c20a35ec421584956
Florian Körner <contact@florian-koerner.com>
2020-08-29 23:19:20 +0200

    Change: use Math.imul

--- packages/avatars/src/prng.ts
+++ packages/avatars/src/prng.ts
@@ -12,8 +12,7 @@ function hashSeed(seed: string) {
   let hash = 0;
 
   for (let i = 0; i < seed.length; i++) {
-    hash = (hash << 5) - hash + seed.charCodeAt(i);
-    hash |= 0;
+    hash = (Math.imul(31, hash) + seed.charCodeAt(i)) | 0;
   }
 
   return hash;
