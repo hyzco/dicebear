6564cd00e6077a0a675917cddc93f515b8645009
Florian Körner <contact@florian-koerner.com>
2020-08-31 21:16:47 +0200

    Change: Reduce packages

--- packages/avatars-open-peeps/rollup.config.js
+++
@@ -1,6 +0,0 @@
-import pkg from './package.json';
-import { getUmdConfig, getCjsAndEsConfig } from '../../build/utils';
-
-export default async () => {
-  return [getUmdConfig(pkg), getCjsAndEsConfig(pkg)];
-};
